105f54f63a309c664d35dd46d550954415092d01
soyeon <sysy0313@gmail.com>
2023-07-28 18:10:59 +0900

    homework

--- script/scale.js
+++ script/scale.js
@@ -2,6 +2,8 @@
 const userHeight = document.querySelector('#user_height');
 const userWeight = document.querySelector('#user_weight');
 const scaleBtn = document.querySelector('#scale_btn');
+const result = document.querySelector('.result')
+const over = document.querySelector('.over_weight')
 let normal_w
 // 변수확인
 console.log(userHeight, userWeight, scaleBtn)
@@ -9,5 +11,7 @@ console.log(userHeight, userWeight, scaleBtn)
 //적정체중 = (본인신장-100)*0.9
 scaleBtn.addEventListener('click', function (){
     normal_w = (userHeight.value - 100)*0.9
-    window.alert(`적정무게는 ${normal_w} kg 입니다. 초과되는 무게는 ${userWeight.value - normal_w} kg 입니다.`)
+    // window.alert(`적정무게는 ${normal_w} kg 입니다. 초과되는 무게는 ${userWeight.value - normal_w} kg 입니다.`)
+    result.innerHTML = `적정무게는 ${normal_w} kg 입니다.`
+    over.innerHTML = `초과되는 무게는 ${userWeight.value - normal_w} kg 입니다.`
 })
